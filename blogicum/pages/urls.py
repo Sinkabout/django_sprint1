5a2f394504fa9e379f7b48603ac09189125abdd5
Дмитрий Фомин <dimafomin2030@mail.ru>
2024-10-20 21:58:00 +0700

    Прописан base.html, скачаны все нужные картинки, дополнен header и footer

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
     path('about/', views.about, name='about.html'),
     path('rules/', views.rules, name='rules.html'),
