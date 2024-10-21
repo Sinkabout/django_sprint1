399637914b454f51ad4a88c52f932ebe80310685
Дмитрий Фомин <dimafomin2030@mail.ru>
2024-10-21 13:24:39 +0700

    Исправление исправлений

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', views.about, name='about.html'),
-    path('rules/', views.rules, name='rules.html'),
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
 ]
